ce96d345e10aa1fb703548315b6e7e8fca5fae7c
Daniel Bennington <dbennington125@gmail.com>
2020-03-25 18:20:04 -0700

    changes were made

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+//step 1
 const myCamera = { name: 'Camera', use: 'heavy', hotkey: 0 };
 const myText = { name: 'Text', use: 'medium', hotkey: 1 };
 const myPhone = { name: 'Phone', use: 'light', hotkey:2 };
 const myBrowser = { name: 'Browser', use: 'heavy', hotkey:3 };
 const myWeather = { name: 'Weather', use: 'none', hotkey: 4 };
+const myGPS ={ name: 'GPS', use: 'heavy', hotkey:5 };
 
-const myApps = [myCamera, myPhone, myText, myBrowser, myWeather];
+//step 2
+const myApps = [myCamera, myPhone, myText, myBrowser, myWeather, myGPS];
 
 
+//step 3
 
 for(let i = 0; i < myApps.length; i++) {
     const myApp = myApps[i];
@@ -16,15 +20,12 @@ for(let i = 0; i < myApps.length; i++) {
 }
 
 
-for(let i = 0; i < myApps.length; i++) {
-    const myApp = myApps[i];
-    console.log('Using my ', i , myApp.name);
-    
-
-}
-
 function logOutThings () {
-    console.log('here are your')
+    console.log('here are your' + myApps.length + 'things')
 
 }
-logOutThings()
+  logOutThings();
+
+//step 4
+
+// function newFunk ()
